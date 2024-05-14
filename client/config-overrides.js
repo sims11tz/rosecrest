4d35543d5c7ddbbdf22b06b28a543c18316c0f0d
dgeurts <dave@fallingstate.com>
2024-05-14 15:14:33 -0600

    ok2234234asdf32

--- client/config-overrides.js
+++ client/config-overrides.js
@@ -1,5 +1,5 @@
 const path = require('path');
-const { override, addBabelPreset, addWebpackModuleRule, useBabelRc } = require('customize-cra');
+const { override, addWebpackAlias, addWebpackModuleRule, addBabelPreset } = require('customize-cra');
 
 module.exports = override(
   // Use Babel preset for TypeScript to handle .ts and .tsx files
@@ -15,25 +15,11 @@ module.exports = override(
           transpileOnly: true  // This option increases build speeds but you will need to run type checks separately
         }
       }
-    //   {
-    //     loader: require.resolve('react-refresh-webpack-plugin/loader'),
-    //     options: {
-    //       skipEnvCheck: true  // This helps avoid issues with environment checks in the loader itself
-    //     }
-    //   }
     ]
   }),
 
   // Setup an alias to simplify imports from the shared directory
-  (config) => {
-    config.resolve.alias = {
-      ...config.resolve.alias,
-      '@shared': path.resolve(__dirname, 'src/shared')
-    };
-    return config;
-  },
-
-  // Optional: Use a .babelrc or babel.config.js if you have custom Babel configurations
-  // Uncomment the line below if you have a custom Babel config you want to use
-  // useBabelRc()
+  addWebpackAlias({
+    '@shared': path.resolve(__dirname, 'src/shared'),
+  })
 );
